a01448c148708433c10646e3e8e10debc7644cc5
David Rowbotham <thedaver64@gmail.com>
2023-12-03 06:12:46 +0000

    figuring out cross platform known folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "abyss.h"
+#include <cstdlib>
 
+void path_test()
+{
+#ifdef _WIN32
+    const char *p = std::getenv("APPDATA");
+    const char *f = "abyss";
+    printf("data folder='%s\\%s'\n", p, f);
+#else
+    char *p = std::getenv("HOME");
+    const char *f = ".abyss";
+    printf("data folder='%s/%s'\n", p, f);
+#endif
+}
 int main(int argc, char *argv[])
 {
-
+    path_test();
     return 0;
 }
